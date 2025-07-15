611576d7eaa49001db1848f72a3b5da4fedae175
Timothee <paultimothylabro@gmail.com>
2025-07-15 10:19:37 +0800

    Responvsive bg JS and blank bg

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const img = document.getElementById("image");
+const mediaQuery = window.matchMedia("(max-width: 810px)");
+
+const updateImage = (e) => {
+    if (e.matches) {
+        img.src = "public/images/mobile_blank.png"
+    }
+    else {
+        img.src = "public/images/web_blank.png"
+    }
+}
+
+updateImage(mediaQuery);
+
+mediaQuery.addEventListener("change", updateImage);
+
 document.getElementById("login").addEventListener("click", () => {
     console.log("Hello world!");
 });
